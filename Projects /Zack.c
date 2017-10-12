949919f0d5b70699288ed1a6cd2a6001fdac6966
Kazi Rahiv <rahivjobs@gmail.com>
2017-10-13 01:13:20 +0600

    muah

---
+++ Projects /Zack.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    printf("Press (1) For Video Download and\n      (2) For Mp3 Download.\n      (3) For a Entire Playlist Download\n");
+    int button;
+    scanf("%d", &button);
+    if(button==1){
+        char ydlv[50]="youtube-dl ";
+        char link[50];
+        printf("Enter link-> ");
+        scanf("%s", link);
+        strcat(ydlv, link);
+        printf("Downloading Video....\n");
+        system(ydlv);
+        }
+    if(button==2){
+        char ydlc[50]="youtube-dl --extract-audio --audio-format mp3 ";
+        char link[50];
+        printf("Enter link-> ");
+        scanf("%s", link);
+        strcat(ydlc, link);
+        printf("Downloading Mp3....\n");
+        system(ydlc);
+        }
+    if(button==3){
+        char ydlp[100]="youtube-dl -t ";
+        char link[100];
+        printf("Enter link-> ");
+        scanf("%s", link);
+        strcat(ydlp, link);
+        printf("Downloading Playlist....\n");
+        system(ydlp);
+        }
+
+
+    }
