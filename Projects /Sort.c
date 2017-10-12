b0a813edea29c31ff49ea3c2902a5691d33baf5a
Kazi Rahiv <rahivjobs@gmail.com>
2017-10-12 14:30:36 +0600

    yao

---
+++ Projects /Sort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[]){
+
+    if(argc<=1){printf("Please give me arguments !");}
+
+    int conv;
+    int ar[argc+1];
+    for(int i=1, j=0; i<argc; i++){
+            conv = atoi(argv[i]);
+            printf("Integer version = %d \n", conv );
+            ar[j] = conv;
+            j++;
+            }
+    sort(ar, argc);
+
+    }
+
+int sort(int ar[], int argc){
+    int temp;
+    for(int j=0; j<50; j++){
+        for(int i=0; i<11; i++){
+        if(ar[i]>ar[i+1]){
+        temp = ar[i];
+        ar[i]= ar[i+1];
+        ar[i+1] = temp;
+        }
+    }
+    }
+    for(int i= 1; i<argc; i++){
+        printf("Reversed Version %d \n", ar[i]);
+    }
+    }
